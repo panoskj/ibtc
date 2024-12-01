86f58856385b22ce794949cb61e7eeacc7f96c69
panoskj <panoskj@mail.com>
2024-12-01 07:57:00 +0200

    Add prettier auto-formatting.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,25 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier/recommended';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+    { files: ['**/*.{js,mjs,cjs,ts}'] },
+    { languageOptions: { globals: globals.browser } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+
+    // Add prettier
+    {
+        ...prettier,
+        rules: {
+            'prettier/prettier': [
+                'warn',
+                {
+                    endOfLine: 'auto',
+                },
+            ],
+        },
+    },
 ];
